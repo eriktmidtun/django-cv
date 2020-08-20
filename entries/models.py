40c52069f3f96eb2ff378eb5918c619f561712a8
Erik Turøy Midtun <etm.midtun@gmail.com>
2020-08-20 14:46:54 +0200

    fixed orderingissue

--- entries/models.py
+++ entries/models.py
@@ -10,30 +10,32 @@ class CommonModel(models.Model):
 
     class Meta:
         abstract = True
-        ordering = ['-order_priority']
-
 
 class Section(CommonModel):
     class Meta:
         verbose_name = 'Section'
+        ordering = ['-order_priority']
 
 class Entry(CommonModel):
     time = models.CharField(max_length=100)
     place = models.CharField(max_length=100, blank=True, null=True)
     description = models.CharField(max_length=200, blank=True, null=True)
-    section = models.ForeignKey(Section, on_delete = models.CASCADE)
+    section = models.ForeignKey(Section, on_delete = models.CASCADE, related_name='entries')
 
     class Meta:
         verbose_name = 'Entrie'
+        ordering = ['-order_priority']
 
 class Sidebar(CommonModel):
     class Meta:
         verbose_name = 'Sidebar section'
+        ordering = ['-order_priority']
 
 class Sentry(CommonModel):
     link = models.CharField(max_length=100, null=True, blank=True)
-    sidebar_section = models.ForeignKey(Sidebar, on_delete = models.CASCADE)
+    sidebar_section = models.ForeignKey(Sidebar, on_delete = models.CASCADE, related_name='sidebar_entries')
     space_bottom = models.BooleanField(default=False)
    
     class Meta:
         verbose_name = 'Sidebar entrie'
+        ordering = ['-order_priority']
--- entries/views.py
+++ entries/views.py
@@ -6,5 +6,10 @@ from .models import Section, Sidebar
 def home(request):
     Sections = Section.objects.exclude(visible=False)
     Sidebars = Sidebar.objects.exclude(visible=False)
+    for sec in Sections.all():
+        print("Section", sec, sec.order_priority)
+        for entry in sec.entries.all():
+            print("entry", entry, entry.order_priority)
+            
 
     return render(request, 'entries/content.html', {'Sections': Sections, 'Sidebars' : Sidebars})
