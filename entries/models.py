3a0ec9dbed5dffcb0763dc3368826f0d51d75152
Erik Turøy Midtun <etm.midtun@gmail.com>
2020-08-20 11:31:02 +0200

    made some fields nullable, sidebar space option

--- entries/models.py
+++ entries/models.py
@@ -14,17 +14,26 @@ class CommonModel(models.Model):
 
 
 class Section(CommonModel):
-    verbose_name = 'Section'
+    class Meta:
+        verbose_name = 'Section'
 
 class Entry(CommonModel):
     time = models.CharField(max_length=100)
-    place = models.CharField(max_length=100)
-    description = models.CharField(max_length=200)
+    place = models.CharField(max_length=100, blank=True, null=True)
+    description = models.CharField(max_length=200, blank=True, null=True)
     section = models.ForeignKey(Section, on_delete = models.CASCADE)
 
+    class Meta:
+        verbose_name = 'Entrie'
+
 class Sidebar(CommonModel):
-    verbose_name = 'Sidebar sections'
+    class Meta:
+        verbose_name = 'Sidebar section'
 
 class Sentry(CommonModel):
     link = models.CharField(max_length=100, null=True, blank=True)
     sidebar_section = models.ForeignKey(Sidebar, on_delete = models.CASCADE)
+    space_bottom = models.BooleanField(default=False)
+   
+    class Meta:
+        verbose_name = 'Sidebar entrie'
